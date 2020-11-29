bc9d007ed0143966d326f1267ced96e02c380be6
dalbodeule <jioo0224@naver.com>
2020-11-30 01:39:08 +0900

    AlarmAddWindow - add UI elements, etc...
    - add UI Elements
    - add Schedule Add logics

--- AlarmAddWindow.py
+++ AlarmAddWindow.py
@@ -3,8 +3,11 @@ import sys
 from PyQt5.QtCore import QDate, QTime
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
+import re, os
 
 UI = uic.loadUiType("./ui/AlarmAddWindow.ui")[0]
+
+TZRegExp = re.compile(r"^((?:\+|\-)\d{2}\d{2})$")
 
 
 class AlarmAddWindow(QMainWindow, UI):
@@ -15,14 +18,48 @@ class AlarmAddWindow(QMainWindow, UI):
         self.AlarmTime.setTime(QTime.currentTime())
         self.AlarmDate.setDate(QDate.currentDate())
 
-        self.addButton.clicked.connect(self.addBtnClicked)
+        self.addButton.clicked.connect(self.add_btn_clicked)
+        self.AlarmTune.clicked.connect(self.tune_btn_clicked)
 
-    def addBtnClicked(self):
+        self.name = ""
+        self.time_str = ""
+        self.date_str = ""
+        self.tune_url = ""
+        self.description = ""
+        self.timezone = ""
+
+    def add_btn_clicked(self):
         time = self.AlarmTime.time()
         date = self.AlarmDate.date()
 
-        print(f"{time.hour()}:{time.minute()}:{time.second()}")
-        print(f"{date.year()}-{date.month()}-{date.day()}")
+        self.time_str = time.toString("HH:mm:ss")
+        self.date_str = date.toString("Y-M-d")
+
+        self.description = self.AlarmDescription.toPlainText()
+
+        self.name = self.AlarmName.text()
+
+        self.timezone = self.AlarmTimezone.text()
+
+        print(self.name, self.time_str, self.date_str, self.tune_url, self.timezone, self.description)
+
+        timezone_result = TZRegExp.search(self.AlarmTimezone.text())
+
+        if not timezone_result:
+            self.statusBar.showMessage(f"{self.timezone} 은 맞지 않는 시간대 문자열 입니다. (+-HHMM)")
+            return
+        else:
+            print(timezone_result.group(1))
+
+    def tune_btn_clicked(self):
+        tune_file = QFileDialog.getOpenFileName(parent=self, caption="알람음 열기", filter="*.wav *.mp3")
+
+        if tune_file[1] is not None:
+            self.tune_url = tune_file[0]
+
+            print(os.path.split(self.tune_url))
+
+            self.AlarmTuneInfo.setText(os.path.split(self.tune_url)[1])
 
 
 if __name__ == '__main__':
