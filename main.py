4ae3c0c90ba41eda9683092600dc15aeae241098
dalbodeule <jioo0224@naver.com>
2020-11-30 12:34:41 +0900

    MainWindow - add Timer (interval: 1000ms)
    - add Timer (interval: 1000ms)
    - add Time show logics (integrated with timer)
    
    - add debug code (if run timer_handler, show current timestamp)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtWidgets import *
-from PyQt5 import uic
+from PyQt5 import uic, QtCore
+import time
 
 UI = uic.loadUiType("./ui/MainWindow.ui")[0]
 
@@ -10,6 +11,18 @@ class MainWindow(QMainWindow, UI):
         super().__init__()
         self.setupUi(self)
 
+        self.Timer = QtCore.QTimer(self)
+        self.Timer.setInterval(1000) # every 1 seconds
+
+        self.Timer.timeout.connect(self.timer_handler)
+
+    def timer_handler(self):
+        current_time = time.localtime()
+
+        self.CurrentTime.setText(time.strftime("%p %I:%M:%S"))
+
+        print(int(time.mktime(current_time)))
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
@@ -17,5 +30,6 @@ if __name__ == '__main__':
     myWindow = MainWindow()
 
     myWindow.show()
+    myWindow.Timer.start()
 
     app.exec_()
